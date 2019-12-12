a22e0cff83ff1d7d07b705cd645eab3df9292e37
unknown <yannvok2@gmail.com>
2019-12-12 11:20:35 +0100

    first commit

---
+++ DataPreprocessing.py
@@ -0,0 +1,257 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities  
+from nltk import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import word2vec
+
+class DataPreProcessing():
+
+    def __init__(self, word2vec_path):
+        self.__toknizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
+        self.__Word2_vec_model = word2vec.load(word2vec_path)
+        
+    def get_rid_of_unwanted_chars(self, corpus):
+        return " ".join(re.findall("[\D.]+", " ".join(re.findall("[\w.]+", corpus))))
+
+    def scrape_google_search(self, query_and_labels, files_path, firefox_path, driver_path, pages):  
+        '''
+        - Function to scrape every link of n pages of a google search. 
+        - In query_and_labels dataframe we have every query that we want to look for in google. We do also have their associated labels
+        - To achieve the scraping, we use selenium (a firefox add-on that has been originally created for website testing) and BeautifulSoup
+        '''
+        cap                 = DesiredCapabilities.FIREFOX.copy() 
+        options             = webdriver.FirefoxOptions()
+        options.binary      = firefox_path
+        cap["marionette"]   = True
+        driver              = webdriver.Firefox(options=options,executable_path=r''+driver_path, capabilities=cap)
+        all_contents        = []
+        for value in query_and_labels.itertuples():     
+            try:
+                print("QUERY {} IS BEING TREATED...".format(value.query))
+                driver.get("https://www.google.fr/search?q="+value.query+"&num="+str(pages*10))
+                html = driver.page_source
+                soup = BeautifulSoup(html)
+                progression=0
+                #1- We get every link on every selected page
+                for div_tags in soup.find_all("div", class_="r"):
+                    a_tags = div_tags.findChildren("a", recursive=False)[0].prettify()
+                    link   = re.findall("https?://[\w./-]+",a_tags)[0]
+                    driver.get(link)
+                    html_link = driver.page_source
+                    soup_link = BeautifulSoup(html_link)
+                    #2- For every link, we get the content 
+                    for div in soup_link.find_all("div", class_="content"):
+                        text = self.get_rid_of_unwanted_chars(div.get_text())
+                        if len(text.strip())>0:
+                            all_contents.append([text, value.label])
+                    progression+=1
+                    print("{}-LINK: {} TREATED".format(progression, link))
+                print("QUERY {} TREATED".format(value.query))
+                progression=0         
+            except (WebDriverException, UnicodeEncodeError):
+                pass
+            #3- We saved everything in a csv file
+            data_scraped = pd.DataFrame(all_contents, columns=["content","label"])
+            data_scraped.to_csv(path_or_buf=files_path, index=False)
+    
+    def train_test_separator(self, content, labels, test_size):
+        '''
+        Train_test splitter with stratify option enabled
+        '''
+        X_train, X_test, y_train, y_test = train_test_split(content, labels, test_size=test_size, stratify=labels)
+        return X_train, X_test, y_train, y_test
+    
+    def sent_tokenizer(self, string):
+        '''
+        Self_made sentence tokenizer because sent_tokenizer of nltk doesn't split properly.
+        Especially if there's no space after a full stop or other punctuations
+        '''
+        return re.split("[?.!]+", re.sub("[?.!]$", "", string))
+    
+    def group_contents_by_label(self, X, y):
+        '''
+        Function to group every single corpus in one corpora for every category
+        '''
+        X_total                = np.concatenate((X.reshape((-1,1)), y.reshape((-1,1))), axis=1)
+        contents_by_categories = defaultdict(list)
+        for row in X_total:
+            content = row[0]
+            label   = row[1]
+            contents_by_categories[label].append(content)
+        return contents_by_categories
+
+    def clean_up_files(self, files_by_label):
+        '''
+        Function to get rid of everything that is not a word: unwanted punctuations, email-adresses and so forth...
+        '''
+        corpus_joined               = ""
+        all_sentences               = ""
+        unique_words                = None
+        files_by_label_cleansed_up  = defaultdict(int)
+        for label, corpora in files_by_label.items():
+            if len(corpora)!=0:
+                for corpus in corpora:
+                    corpus_joined+="".join(corpus.lower())
+                corpus_joined_tokenized             = self.sent_tokenizer(self.get_rid_of_unwanted_chars(corpus_joined))
+                files_by_label_cleansed_up[label]   = corpus_joined_tokenized
+                corpus_joined=""
+                all_sentences+=" ".join(corpus_joined_tokenized)
+        unique_words=list(sorted(set(self.__toknizer.tokenize(self.get_rid_of_unwanted_chars(all_sentences)))))
+        return files_by_label_cleansed_up, unique_words
+
+    def hist_corpus_per_label(self, files_by_label, path_save):
+        '''
+        Plot number of corpus per label
+        '''
+        #How many files per label
+        x = list(files_by_label.keys())
+        length_files_by_label=0 
+        for corpus in list(files_by_label.keys()):
+            length_files_by_label+=len(files_by_label[corpus])    
+        y = [round((len(files_by_label[corpus])/length_files_by_label)*100,2) for corpus in list(files_by_label.keys())]
+        plt.figure(figsize=(300,150)) #width:300  height:150
+        plt.bar(x,y, align="center") # A bar chart
+        plt.xlabel('Labels', fontsize=50, labelpad=40)
+        plt.xticks(fontsize=150, rotation="vertical")
+        plt.ylabel('Corpora', fontsize=50, labelpad=40)
+        plt.yticks(fontsize=70)
+        plt.title("Nombre de corpus par label", fontsize=100)
+        for i in range(len(y)):
+            plt.annotate(s=y[i],xy=(x[i], y[i]+1), fontsize=100)
+            plt.hlines(y[i], xmin=0, xmax=0)         
+        plt.savefig(path_save)
+        plt.show()
+  
+    def tf_and_idf(self, files_by_label_cleansed_up):
+        '''
+        Term frequency and Inverse Document Frequency
+        TF  = occurence of word w in a corpora
+        IDF = log10(Total number of documents/ Number of documents with word w in it)   
+        '''
+        tf_dict                 = defaultdict(int) #term frequency of words per label
+        N_doc_with_each_term    = defaultdict(int) #Number of documents(corpora) with word w in it 
+        word_counts             = defaultdict(int) #unique_words_per_label
+    
+        for label, corpora in files_by_label_cleansed_up.items():
+            #tf
+            for sentence in corpora: 
+                for word in self.__toknizer.tokenize(re.sub("[.]"," ",sentence)):
+                    word_counts[word]+=1  
+            tf_dict[label] = word_counts
+            #idf
+            for word in word_counts.keys():
+                N_doc_with_each_term[word]+=1     
+            word_counts = defaultdict(int)
+        idf_dict={key: round(np.log10(len(files_by_label_cleansed_up.keys())/value), 2) for key, value in N_doc_with_each_term.items()}
+        return tf_dict, idf_dict
+    
+    def cosine_similarity(self, list_of_words, cos_z_score_threshold, verbose=False):
+        '''
+        cosine similarity matrix of every word in a tf_idf[category] dictionary
+        '''
+        words=[]
+        words_left_out=[]
+        for word in list_of_words:
+            #Checking if a vector for a given word exists. If it doesn't we discard the word
+            try:   
+                self.__Word2_vec_model.get_vector(word)
+                words.append(word)
+            except KeyError:
+                words_left_out.append(word)     
+        words_bis=words.copy()
+        words_bis.append("TOTAL")
+        #Initializing the cosine similarity matrix
+        cosine_sim_matrix = pd.DataFrame({}, index=words, columns=words_bis)
+        for counter, row in enumerate(cosine_sim_matrix.itertuples()):
+            index   = row[0]
+            cosines = []
+            for word in cosine_sim_matrix.columns[:-1]:    
+                index_vector    = self.__Word2_vec_model.get_vector(index)
+                word_vector     = self.__Word2_vec_model.get_vector(word)
+                cos             = np.dot(index_vector, word_vector)/(np.linalg.norm(index_vector)*np.linalg.norm(word_vector)) 
+                #Cos is a function that has values ranging from [-1,1] we will only take into account values that are in [0,1]
+                if cos<0:
+                    cos=0
+                cosine_sim_matrix.loc[index, word] = cos
+                if index!=word:
+                    cosines.append(cos)
+            #We compute the mean of similarities. The greater it is the more similar the word is with other words.
+            cosine_sim_matrix.loc[index, cosine_sim_matrix.columns[-1]] = np.mean(cosines)
+            if verbose==True:
+                print(str(counter)+" "+index+" treated")           
+        #z_scores allow us to choose between discarding words that have no so similarity with others (for a given category) whatsoever(outliers) and those that are relatively a bit closer.   
+        #positive z_score means that we only want to keep similar words
+        z_scores=(cosine_sim_matrix.TOTAL - np.mean(cosine_sim_matrix.TOTAL))/np.std(cosine_sim_matrix.TOTAL)
+        words_to_discard = list(z_scores[z_scores<cos_z_score_threshold].index)
+        return words_to_discard, words_left_out
+
+    def tf_idf_scores(self, tf_dict, idf_dict, tf_idf_z_score_threshold, cos_z_score_threshold, verbose=False): 
+        '''
+        tf_idf = tf * idf : it tells us how important a word is to a category
+        But we need a second filter for our words: the cosine similarity
+        We need words that are both important to a category and similar to other words for that same category 
+        ''' 
+        tf_idf_dict=defaultdict(int) 
+        tf_idf_scores=defaultdict(int)
+        for dict_label, documents in tf_dict.items():
+            if verbose==True:
+                print("\n***********{} IS BEING TREATED***********".format(dict_label))
+            for word, term_frequency in documents.items():
+                tf_idf_scores[word]=term_frequency*idf_dict[word]  
+            #First filter:get the rarest words(the most influential ones) by keeping words that have a z_score greater than tf_z_score_threshold
+            tf_idf_scores={word: (value - np.mean(list(tf_idf_scores.values()))) / np.std(list(tf_idf_scores.values())) for word, value in tf_idf_scores.items() if (value - np.mean(list(tf_idf_scores.values()))) / np.std(list(tf_idf_scores.values())) > tf_idf_z_score_threshold}
+            
+            #Second filter: get the words that are similar to one another 
+            list_words=list(tf_idf_scores.keys())
+            words_to_discard, words_left_out=self.cosine_similarity(list_words, cos_z_score_threshold, verbose)
+            tf_idf_scores={word: value for word, value in tf_idf_scores.items() if word not in words_to_discard and word not in words_left_out}
+            tf_idf_dict[dict_label]=tf_idf_scores
+            tf_idf_scores=defaultdict(int)
+        return tf_idf_dict
+     
+    def shrink_sentence(self, context_word, sentence, window):
+        '''
+        Function that shrink a sentence around a context word by a given a window size
+        '''
+        sent_index   = sentence.index(context_word)
+        sentence_1   = " ".join(sentence[:sent_index].split()[-window:])
+        sentence_2   = " ".join(sentence[sent_index+len(context_word):].split()[:window])
+        new_sentence = "{} {} {}".format(sentence_1, context_word, sentence_2)
+        return new_sentence
+
+    def score_sentences(self, files_by_label_cleansed_up, tf_idf_dict):
+        '''
+        Scores every sentence for a given category.
+        If the score exceeds the mean of all scores(for a given category), we keep the sentence in our final training set
+        If the sentence is selected, we then need to shrink it around a specific context word (present in tf_idf or related to one of the words in a tf_idf)
+        '''
+        dict_scores=defaultdict(int)
+        for label, sentences in files_by_label_cleansed_up.items():
+            sentences_score_per_label=[]
+            scores_per_label=[]
+            for sentence in sentences:
+                sentences_shrunk=[]
+                n_words_per_sentence=0
+                sentence_score=0
+                for word in self.__toknizer.tokenize(sentence):
+                    if word in tf_idf_dict[label].keys():
+                        n_words_per_sentence+=1
+                        word_score=tf_idf_dict[label][word]
+                        sentence_score+=word_score
+                        shrunk_sent=self.shrink_sentence(word, sentence, 3)
+                        if shrunk_sent not in sentences_shrunk:
+                            sentences_shrunk.append(shrunk_sent)
+                if n_words_per_sentence!=0:
+                    sentence_score_normalized=sentence_score/n_words_per_sentence
+                    scores_per_label.append(sentence_score_normalized)
+                    for sentence_shrunk in sentences_shrunk:
+                        sentences_score_per_label.append((sentence_shrunk, sentence_score_normalized))
+            dict_scores[label]=[sentence for sentence in sentences_score_per_label if sentence[1] > np.mean(scores_per_label)]       
+        return dict_scores
---
+++ Model.py
@@ -0,0 +1,253 @@
+import pandas as pd
+import numpy as np
+import word2vec
+from sklearn.utils import shuffle
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dropout
+import tensorflow as tf
+from collections import defaultdict
+from nltk import RegexpTokenizer
+from sklearn.preprocessing import OneHotEncoder
+import operator
+import re
+import pickle
+
+class ModelBuilder:
+    
+    def __init__(self, word2vec_path):
+        self.__toknizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
+        self.__Word2_vec_model = word2vec.load(word2vec_path)
+        self.__one_hot_encoder = OneHotEncoder()
+              
+    def get_word_2_vec_model(self):
+        return self.__Word2_vec_model
+    
+    def get_tokenizer(self):
+        return self.__toknizer
+    
+    def get_one_hot_encoder(self):
+        return self.__one_hot_encoder
+    
+    def sent_tokenizer(self, string):
+        '''
+        Self_made sentence tokenizer because sent_tokenizer of nltk doesn't split properly.
+        Especially if there's no space after a full stop or other punctuations
+        '''
+        return re.split("[?.!]+", re.sub("[?.!]$", "", string))
+
+    def get_rid_of_unwanted_chars(self, corpus):
+        return " ".join(re.findall("[\D.]+", " ".join(re.findall("[\w.]+", corpus))))
+    
+    def sentence_to_vector(self, X_not_encoded):
+        '''
+        Function to implement sentence embedding 
+        '''
+        #We use word2Vec embedding, if there is a vector for a given word, we use its vector, otherwise we discard it.
+        X_encoded=[] 
+        for sentence in X_not_encoded:
+            sent_vector=0
+            for word in self.__toknizer.tokenize(sentence):
+                try:
+                    sent_vector+=self.__Word2_vec_model.get_vector(word)
+                except KeyError:
+                    pass
+            X_encoded.append(sent_vector)
+        X_encoded=np.array(X_encoded)
+        return X_encoded
+                  
+    def build_training_set(self, contents_by_categories):
+        '''
+        Encoding, shuffling and gathering everything up 
+        '''
+        #Creating not_encoded X_train and y_train datasets from contents_by_categories
+        X_train_not_encoded=[]
+        y_train_not_encoded=[]
+        for index, corpora in contents_by_categories.items():
+            for corpus in corpora:
+                X_train_not_encoded.append(corpus[0])
+                y_train_not_encoded.append(index)
+        
+        X_train_not_encoded=np.array(X_train_not_encoded)
+        y_train_not_encoded=np.array(y_train_not_encoded)
+        
+        #Encoding our training_set contents and labels 
+        X_train_encoded=self.sentence_to_vector(X_train_not_encoded)
+        
+        #Encoding our training_set labels 
+        y_train_encoded=[] 
+        y_train_not_encoded=y_train_not_encoded.reshape((-1,1))
+        self.__one_hot_encoder.fit(y_train_not_encoded)
+        y_train_encoded = self.__one_hot_encoder.transform(y_train_not_encoded).toarray()
+        
+        #Shuffling training_set 
+        X_and_y=np.concatenate((X_train_encoded, y_train_encoded), axis=1)
+        X_and_y=shuffle(X_and_y)      
+        X_train_encoded=X_and_y[:,:X_train_encoded.shape[1]]
+        y_train_encoded=X_and_y[:,X_train_encoded.shape[1]:]
+        return X_train_encoded, y_train_encoded
+       
+    def neural_network(self, X, y, path, epoch, batch_size, lr, validation_split):
+        model = tf.keras.Sequential()
+        model.add(layers.Dense(units=X.shape[1] , input_dim=X.shape[1]))
+        model.add(Dropout(0.2))
+        model.add(layers.Dense(units=400, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(layers.Dense(units=200, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(layers.Dense(units=100, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(layers.Dense(units=50, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(layers.Dense(units=y.shape[1], activation='softmax'))
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=[tf.keras.metrics.Precision()], lr=lr, validation_split=validation_split) 
+        model.fit(X, y, epochs=epoch, batch_size=batch_size)
+        model.save(path)
+        print("Saved model to "+str(path))
+        return model
+      
+    def cosine_similarity(self, word, tf_idf_scores):
+        '''
+        cosine similarity of every word with a given word in tf_idf[category]
+        '''
+        try:
+            word_vector=self.__Word2_vec_model.get_vector(word)
+            cosines_dict=defaultdict(int) 
+            tf_idf_words=tf_idf_scores.keys()  
+            for tf_idf_word in tf_idf_words:  
+                tf_idf_word_vector=self.__Word2_vec_model.get_vector(tf_idf_word)
+                cos=np.dot(word_vector, tf_idf_word_vector)/(np.linalg.norm(word_vector)*np.linalg.norm(tf_idf_word_vector))   
+                if cos<0:
+                    cos=0
+                cosines_dict[tf_idf_word]=cos
+            td_idf_max_value=np.max(list(cosines_dict.values()))
+            tf_idf_word_of_max_value=max(cosines_dict.items(), key=operator.itemgetter(1))[0]  
+            #if cosine is greater than 0.60, we keep the word
+            if td_idf_max_value > 0.60:
+                return tf_idf_scores[tf_idf_word_of_max_value]
+        except KeyError:
+            return 0
+        return 0
+
+    def shrink_sentence(self, context_word, sentence, window):
+        sent_index=sentence.index(context_word)
+        sentence_1=" ".join(sentence[:sent_index].split()[-window:])
+        sentence_2=" ".join(sentence[sent_index+len(context_word):].split()[:window])
+        new_sentence="{} {} {}".format(sentence_1, context_word, sentence_2)
+        return new_sentence
+    
+    def get_meaningful_sentences_only_with_label(self, tf_idf_dict, X_test, y_test):
+        '''
+        This function has to be applied on a test set
+        
+        1- Get sentences that are relevant. If a sentence has the potential to be classified in any of our category, we keep it. 
+        So we have to see how relevant its words are for any potential category
+        
+        2- Shrink sentences to reduce noise
+        '''
+        X=[]
+        y=[]
+        iterator=1
+        for corpus, categories in zip(X_test, y_test):
+            corpus_=""
+            print("RELEVANT SENTENCES OF CORPUS {} ARE BEING RETRIEVED".format(iterator))
+            clean_corpus=self.get_rid_of_unwanted_chars(corpus)
+            for sentence in self.sent_tokenizer(clean_corpus):
+                sentences_shrunk=[]
+                sentence_score=0
+                #We try to see if perhaps a sentence is relevant to any category
+                for word in self.__toknizer.tokenize(sentence):
+                    #We have to consider every word in order to see how they are related to words in tf_idf[category]
+                    score=0 
+                    for label, tf_idf_scores in tf_idf_dict.items():
+                        try:
+                            score=tf_idf_scores[word]
+                            sentence_score+=score
+                        except KeyError:
+                            score=self.cosine_similarity(word, tf_idf_scores)
+                            sentence_score+=score
+                        if score>0:
+                            sentences_shrunk.append("{}. ".format(self.shrink_sentence(word, sentence, 3)))
+                if sentence_score>0:
+                    corpus_+=" ".join(list(dict.fromkeys(sentences_shrunk)))
+            if len(corpus_.strip())>0:
+                X.append(corpus_)
+                y.append(categories)
+            iterator+=1
+        return np.array(X), np.array(y)
+    
+    def get_only_meaningful_sentences_without_label(self, tf_idf_dict, X):
+        '''
+        This function has to be used for a new observation
+        '''
+        X_new=[]
+        iterator=1
+        for corpus in X:
+            corpus_=""
+            print("RELEVANT SENTENCES OF CORPUS {} ARE BEING RETRIEVED".format(iterator))
+            clean_corpus=self.get_rid_of_unwanted_chars(corpus)
+            for sentence in self.sent_tokenizer(clean_corpus):
+                sentences_shrunk=[]
+                sentence_score=0
+                for word in self.__toknizer.tokenize(sentence):
+                    score=0 
+                    for label, tf_idf_scores in tf_idf_dict.items():
+                        try:
+                            score=tf_idf_scores[word]
+                            sentence_score+=score
+                        except KeyError:
+                            score=self.cosine_similarity(word, tf_idf_scores)
+                            sentence_score+=score
+                        if score>0:
+                            sentences_shrunk.append("{}. ".format(self.shrink_sentence(word, sentence, 3)))          
+                if sentence_score>0:
+                    corpus_+=" ".join(list(dict.fromkeys(sentences_shrunk)))
+            if len(corpus_.strip())>0:
+                X_new.append(corpus_)
+            iterator+=1
+        return np.array(X_new)
+              
+    def predict(self, X, stat_model, categories):
+        '''
+        We predict a label for every sentence in a corpus and we average all the predictions
+        '''
+        all_predictions=[]
+        max_predictions=[]
+        for counter, corpus in enumerate(X):
+            print("PREDICTIONS FOR CORPUS "+str(counter+1)+" ARE BEING PRODUCED")
+            df_prediction=pd.DataFrame(np.zeros(len(categories)), index=categories, columns=["Predictions"])
+            corpus_length=0
+            for sentence in self.sent_tokenizer(corpus.lower()):
+                sentence_vector=0
+                for word in self.__toknizer.tokenize(sentence):
+                    try:   
+                        sentence_vector+=self.__Word2_vec_model.get_vector(word)  
+                    except KeyError:
+                        pass
+                if type(sentence_vector)!=int:
+                    corpus_length+=1
+                    prediction_vector=stat_model.predict(sentence_vector.reshape(1,-1)).T    
+                    df_prediction+=pd.DataFrame(prediction_vector*100, index=categories, columns=["Predictions"])
+            if df_prediction is not None:
+                df_prediction/=corpus_length 
+                max_predictions.append([df_prediction.idxmax()[0], df_prediction.max()[0]])
+                all_predictions.append(df_prediction)
+        return np.array(max_predictions), all_predictions
+    
+    def save_variables(self, path, variable):
+        '''
+        Save our relevant variables with pickle
+        '''
+        with open(path, "wb") as file:
+            pickle.dump(variable, file)
+    
+    def load_variables(self, path):
+        '''
+        Load our variables with pickle
+        '''
+        loaded_variable=None
+        with open(path, "rb") as file:
+            loaded_variable=pickle.load(file)
+        return loaded_variable
+    
+
+
